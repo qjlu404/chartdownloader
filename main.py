a4d5ebed80aaa619d4293629f7ac5f9e0f945d8d
qjlu404 <qjlu404@gmail.com>
2021-01-10 23:04:48 -0600

    It finally downloads PDFs!

--- main.py
+++ main.py
@@ -1,48 +1,79 @@
+from pdf2image import convert_from_path
 from urllib.request import urlopen
 from lxml import etree
+import wget
+import os
+
 url = "https://www.faa.gov/air_traffic/flight_info/aeronav/digital_products/dtpp/search/results/?cycle=2014&ident="
 errormsg = "Something Went Wrong!"
 noLoop = False
 
 
-def downloadchart(name, site):
-    pass
+
+def dload(name, link, type, icao):
+    ffile = type + "." + name
+    print(ffile)
+    wget.download(link, './' + icao + "/" + ffile + ".pdf" )
 
 
-def getdata(furl):
+def getdata(furl, icao):
     response = urlopen(furl)
     htmlparser = etree.HTMLParser()
     tree = etree.parse(response, htmlparser)
+    infohtml = tree.xpath("//table[@id='resultsTable']/tbody/tr/td[last()-2]")
     nameshtml = tree.xpath("//table[@id='resultsTable']/tbody/tr/td/a")
     linkshtml = tree.xpath("//table[@id='resultsTable']/tbody/tr/td/a/@href")
+
     names = []
     links = []
+    types = []
 
     for name in nameshtml:
         names.append(name.text)
+
     for link in linkshtml:
         links.append(link)
-    items = dict(zip(names, links))
 
-    for item in items:
-        print(item, '->', items[item])
+    for info in infohtml:
+        if info.text == 'IAP':
+            types.append('APP')
+
+        elif info.text == 'DP':
+            types.append('DPP')
+
+        elif info.text == 'STAR':
+            types.append('ARR')
+
+        else:
+            types.append('INF')
+
+    for i, j, k in zip(names, links, types):
+        dload(i, j, k, icao)
 
 
 def enter():
     loop = True
     while loop:
+        global icao
         icao = input("Enter ICAO: ")
+        page = input("Enter page number (faa website has multiple pages for data so just choose 1 then next time choose 2 for the next set of data): ")
 
         if len(icao) == 0:
             print("Please enter an ICAO")
+
+        elif page == 0 or len(page) == 0:
+            print("Please enter a page number from 1 and up until you get no more charts.")
 
         elif len(icao) > 4 or len(icao) < 4:
             print("ICAO must be 4 characters\n")
 
         else:
-            furl = url + icao
+            furl = url + icao + "&page=" + page
             loop = False
-            getdata(furl)
+            path = icao
+            if not os.path.exists(path):
+                os.makedirs(path)
+            getdata(furl, icao)
 
 
 if not noLoop:
