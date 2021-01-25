813824bef0f00a623c1fb6f7a14ef5d4cb52be78
qjlu404 <qjlu404@gmail.com>
2021-01-25 09:28:55 -0600

    included default options

--- main.py
+++ main.py
@@ -10,16 +10,19 @@ def main():
     def convert(aicao, achoice):
         tkinter.messagebox.showinfo("Alert", "Click OK to download - Program may become unresponsive for some time")
         n = 1
-        while n < 5:
+        while n < 8:
             dl(aicao, n, achoice)
             n += 1
         tkinter.messagebox.showinfo("Alert", "Finished!")
 
     window.title("ChartDownloader")
-    icao = tkinter.Entry(window)
+    i = tkinter.StringVar(window, value='KSAT')
+    icao = tkinter.Entry(window, textvariable=i)
     window.configure(bg='grey')
     tkinter.Label(window, bg='grey', fg='white', text="Enter ICAO: ", font="Verdana 10 bold").grid(row=1, sticky=tkinter.W)
     choice = tkinter.IntVar()
+    
+    choice.set(1)
     tkinter.Radiobutton(window, bg='grey', text="PDF format", padx=20, variable=choice, value=2).grid(sticky=tkinter.W)
     tkinter.Radiobutton(window, bg='grey', text="Aerobask (PNG) format", padx=20, variable=choice, value=1).grid(sticky=tkinter.W)
     tkinter.Button(window, bg='grey', text='download', command=lambda: convert(icao.get(), choice.get())).grid(row=4, column=1, sticky=tkinter.W, pady=4)
